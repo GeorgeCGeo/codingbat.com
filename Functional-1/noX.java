45a4fb2ee650b46a0cf9ecd211917ae1ad5a3c32
George Georgiadis <george.georgiadis90@gmail.com>
2020-09-07 16:10:02 +0300

    Functional-1 solutions

---
+++ Functional-1/noX.java
@@ -0,0 +1,6 @@
+//Given a list of strings, return a list where each string has all its "x" removed.
+
+public List<String> noX(List<String> strings) {
+  strings.replaceAll(s -> s.replace("x", ""));
+  return strings;
+}
